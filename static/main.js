1d43c648821543327ae9a04bce6b17ceb3f7558c
efindus <48939809+efindus@users.noreply.github.com>
2023-01-14 21:28:56 +0100

    Rewrite
    Signed-off-by: efindus <48939809+efindus@users.noreply.github.com>

--- static/main.js
+++ static/main.js
@@ -1,148 +1,138 @@
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/sw.js').then(reg => {
+if ('serviceWorker' in navigator)
+	navigator.serviceWorker.register('/sw.js');
 
-		if(reg.installing) {
-			console.log('Service worker installing')
-		} else if(reg.waiting) {
-			console.log('Service worker installed')
-		} else if(reg.active) {
-			console.log('Service worker active')
+const boxContainer = document.querySelector('.box');
+const title = document.querySelector('.title');
+const restart = document.getElementById('restart');
+
+// If we didn't set the width safari wouldn't render the svg...
+const circleSvg = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="100" viewBox="0 0 1 1">
+	<circle cx="0.5" cy="0.5" r="0.35" stroke="black" stroke-width="0.05" fill="none"></circle>
+</svg>`;
+const crossSvg = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="100" viewBox="0 0 1 1">
+	<line x1="0.15" y1="0.15" x2="0.85" y2="0.85" stroke="black" stroke-width="0.05"></line>
+	<line x1="0.15" y1="0.85" x2="0.85" y2="0.15" stroke="black" stroke-width="0.05"></line>
+</svg>`;
+
+let currentSide = true;
+let board = [], tiles = [], handlers = [];
+
+const win = (side) => {
+	if (side === 2)
+		title.innerHTML = 'Draw';
+	else
+		title.innerHTML = side ? 'X won' : 'O won';
+
+	for (const h of handlers) {
+		h.item.removeEventListener('click', h.handler);
+		h.item.className = '';
+	}
+};
+
+const checkForWin = () => {
+	for (let x = 0; x < 3; x++) {
+		let curr = 0;
+		for (let i = 3 * x; i < 3 + 3 * x; i++) {
+			if (i === 3 * x) {
+				curr = board[i];
+			} else {
+				if (board[i] !== curr) {
+					curr = 0;
+					break;
+				}
+			}
 		}
 
-	}).catch(error => {
-		console.log('Registration failed with ' + error)
-	})
-}
+		if(curr !== 0)
+			return curr;
+	}
 
-const restart = document.getElementById('restart')
+	for (let x = 0; x < 3; x++) {
+		let curr = 0;
+		for (let i = x; i < 9; i += 3) {
+			if (i === x) {
+				curr = board[i];
+			} else {
+				if (board[i] !== curr) {
+					curr = 0;
+					break;
+				}
+			}
+		}
+
+		if(curr !== 0)
+			return curr;
+	}
+
+	for (let x = 0; x < 3; x += 2) {
+		let curr = 0;
+		for (let i = x; i < (!x ? 9 : 8); i += !x ? 4 : 2) {
+			if (i === x) {
+				curr = board[i];
+			} else {
+				if (board[i] !== curr) {
+					curr = 0;
+					break;
+				}
+			}
+		}
+
+		if(curr !== 0)
+			return curr;
+	}
+
+	let filled = true;
+	for (let i = 0; i < 9; i++) {
+		if (board[i] === 0) {
+			filled = false;
+			break;
+		}
+	}
+
+	if (filled)
+		return 3;
+
+	return 0;
+};
+
+const setup = () => {
+	title.innerHTML = 'Tic Tac Toe';
+	boxContainer.innerHTML = '';
+
+	for (let i = 0; i < 3 ** 2; i++)
+		boxContainer.appendChild(document.createElement('div'));
+
+	tiles = [ ...boxContainer.children ];
+	board = Array(9).fill(0);
+	currentSide = true;
+};
 
 const main = () => {
-    const box = document.getElementById('box'), name = document.getElementById('name')
-    let currentSide = true
-    const circle = `
-    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 1 1">
-        <circle cx="0.5" cy="0.5" r="0.35" stroke="black" stroke-width="0.05" fill="none" />
-    </svg>`
-    const cross = `
-    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 1 1">
-        <line x1="0.15" y1="0.15" x2="0.85" y2="0.85" stroke="black" stroke-width="0.05" />
-        <line x1="0.15" y1="0.85" x2="0.85" y2="0.15" stroke="black" stroke-width="0.05" />
-    </svg>`
+	setup();
 
-    const board = []
-    const handlers = []
-    box.innerHTML = ''
-    name.innerHTML = 'Tic Tac Toe'
+	handlers = [];
+	for (let i = 0; i < 9; i++) {
+		const handler = () => {
+			tiles[i].innerHTML = currentSide ? crossSvg : circleSvg;
+			tiles[i].removeEventListener('click', handler);
+			tiles[i].className = '';
 
-    const win = (side) => {
-        if (side === 2) {
-            name.innerHTML = 'Draw'
-        } else {
-            name.innerHTML = side ? 'O won' : 'X won'
-        }
-        
-        for(const h of handlers) {
-            h.item.removeEventListener('click', h.handler)
-            h.item.className = ''
-        }
-    }
+			board[i] = currentSide + 1, currentSide = !currentSide;
 
-    const checkForWin = () => {
-        for (let x = 0; x < 3; x++) {
-            let curr = 0
-            for (let i = 3 * x; i < 3 + 3 * x; i++) {
-                if (i === 3 * x) {
-                    curr = board[i]
-                } else {
-                    if (board[i] !== curr) {
-                        curr = 0
-                        break
-                    }
-                }
-            }
+			const score = checkForWin();
+			if (score)
+				win(score - 1);
+		}
 
-            if(curr !== 0) {
-                return curr
-            }
-        }
+		handlers.push({
+			item: tiles[i],
+			handler,
+		});
 
-        for (let x = 0; x < 3; x++) {
-            let curr = 0
-            for (let i = x; i < 9; i += 3) {
-                if (i === x) {
-                    curr = board[i]
-                } else {
-                    if (board[i] !== curr) {
-                        curr = 0
-                        break
-                    }
-                }
-            }
-
-            if(curr !== 0) {
-                return curr
-            }
-        }
-
-        for (let x = 0; x < 3; x += 2) {
-            let curr = 0
-            for (let i = x; i < (!x ? 9 : 8); i += !x ? 4 : 2) {
-                if (i === x) {
-                    curr = board[i]
-                } else {
-                    if (board[i] !== curr) {
-                        curr = 0
-                        break
-                    }
-                }
-            }
-
-            if(curr !== 0) {
-                return curr
-            }
-        }
-
-        let filled = true
-        for (let i = 0; i < 9; i++) {
-            if (board[i] === 0) {
-                filled = false
-                break
-            }
-        }
-
-        if (filled) {
-            return 3
-        }
-
-        return 0
-    }
-
-    for (let i = 0; i < 9; i++) {
-        board.push(0)
-        box.innerHTML += `<div id="element-${i}" class="active"></div>`
-    }
-
-    for (let i = 0; i < 9; i++) {
-        const curr = document.getElementById(`element-${i}`)
-        const handler = () => {
-            curr.innerHTML = currentSide ? cross : circle
-            currentSide = !currentSide
-            curr.removeEventListener('click', handler)
-            curr.className = ''
-            board[i] = currentSide + 1
-            const score = checkForWin()
-            if (score) win(score - 1)
-        }
-        handlers.push({
-            item: curr,
-            handler,
-        })
-        curr.addEventListener('click', handler)
-    }
+		tiles[i].classList.add('active');
+		tiles[i].addEventListener('click', handler);
+	}
 }
 
-restart.addEventListener('click', () => {
-    main()
-})
-
-main()
+main();
+restart.onclick = () => main();
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const versionNumber = 'v4'
+const versionNumber = 'v5';
 
 self.addEventListener('install', event => {
 	event.waitUntil(
@@ -16,33 +16,33 @@ self.addEventListener('install', event => {
 				'/static/main.js',
 				'/static/main.css',
 				'/static/manifest.json',
-				'/static/Roboto.ttf',
-			])
+				'/static/Roboto.woff2',
+			]);
 		})
-	)
-})
+	);
+});
 
 self.addEventListener('fetch', event => {
 	event.respondWith(caches.match(event.request).then(response => {
 		if (response !== undefined) {
-			return response
+			return response;
 		} else {
 			return fetch(event.request).then(response => {
-				let responseClone = response.clone()
+				let responseClone = response.clone();
 				
 				caches.open(versionNumber).then(cache => {
-					cache.put(event.request, responseClone)
-				})
-				return response
+					cache.put(event.request, responseClone);
+				});
+				return response;
 			}).catch(() => {
-				return caches.match('/static/logo.jpg')
-			})
+				return caches.match('/static/logo.png');
+			});
 		}
-	}))
-})
+	}));
+});
 
 self.addEventListener('activate', event => {
-	const cacheKeeplist = [ versionNumber ]
+	const cacheKeeplist = [ versionNumber ];
 
 	event.waitUntil(
 		caches.keys().then(keyList => {
@@ -50,7 +50,7 @@ self.addEventListener('activate', event => {
 				if (cacheKeeplist.indexOf(key) === -1) {
 					return caches.delete(key);
 				}
-			}))
+			}));
 		})
-	)
-})
+	);
+});
